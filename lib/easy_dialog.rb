99ed09645a7c44e273dcfe7c3175a0adc520b7b3
juntaki <me@juntaki.com>
2013-12-05 07:39:49 +0900

    first commit

---
+++ lib/easy_dialog.rb
@@ -0,0 +1,57 @@
+require "easy_dialog/version"
+require "curses"
+
+module EasyDialog
+  class Choises
+    def initialize question, choises
+      raise unless question.class == String
+      raise unless choises.class == Array
+      @question = question
+      @choises = choises
+    end
+    def ask
+      width =  @choises.max_by {|s| s.length}.length + 1
+      num = 80 / width
+      highlight = 0
+      Curses.noecho
+      Curses.init_screen
+      Curses.start_color
+      Curses.stdscr.keypad(true)
+      Curses.init_pair 1, Curses::COLOR_BLACK, Curses::COLOR_RED
+      loop do
+        Curses.setpos 0, 0
+        Curses.attroff(Curses::A_COLOR)
+        Curses.addstr(@question)
+        @choises.each_with_index do |c,i|
+          Curses.setpos i/num + 1, i*width
+          if i == highlight
+            Curses.attrset(Curses.color_pair(1))
+          else
+            Curses.attroff(Curses::A_COLOR)
+          end
+          Curses.addstr(c)
+        end
+        Curses.refresh
+        case hoge = Curses.getch
+        when Curses::Key::RIGHT
+          highlight = highlight + 1
+        when Curses::Key::LEFT
+          highlight = highlight - 1
+        when Curses::Key::UP
+          highlight = highlight - num
+        when Curses::Key::DOWN
+          highlight = highlight + num
+        when 27
+          exit 1
+        when 10
+          Curses.close_screen
+          return @choises[highlight]
+        end
+        if highlight >= @choises.length
+          highlight = @choises.length - 1
+        end
+        highlight = 0 if highlight < 0
+      end
+    end
+  end
+end
